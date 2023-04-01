52b1c3f0a38a34dd23efa8fe76c0829f75712caf
Michal Kovář <michalkvr.dev@gmail.com>
2023-04-01 14:48:34 +0200

    feat: configured redux toolkit

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,5 @@ export * from './CommentType';
 export * from './ErrorResponseType';
 export * from './TokenType';
 export * from './TenantType';
+export * from './UserType';
+export * from './CredentialsType';
